5b029196bd0a0df1019875e96399a877d9823f59
Joseph Surla <josephsurla@gmail.com>
2021-10-01 14:37:47 +0900

    update contract to allow users to send messages

--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,24 @@
+const { ethers } = require("hardhat");
+
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
+  console.log("Contact address:", waveContract.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave();
+  let waveTxn = await waveContract.wave("Hello there!");
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
-
-  waveTxn = await waveContract.connect(randomPerson).wave();
+  const [_, randomPerson] = await ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Hi!");
   await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
